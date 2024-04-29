5f01bc0de4c945cc2b2fac9428d09f8a4bcbac9a
Frengky Gunawan <frengky.gunawan@erajaya.com>
2024-04-29 15:21:56 +0700

    feat: I Am Poor App

---
+++ 02 I-Am-Poor/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Frengky Gunawan on 29/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
