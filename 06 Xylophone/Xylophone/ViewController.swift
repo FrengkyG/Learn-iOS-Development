db7b11a9ca51e5d6c6ecdf65e6c739d8f39eac33
Frengky Gunawan <frengky.gg@gmail.com>
2024-05-07 23:16:56 +0700

    feat: add touch effect

--- 06 Xylophone/Xylophone/ViewController.swift
+++ 06 Xylophone/Xylophone/ViewController.swift
@@ -11,21 +11,24 @@ import AVFoundation
 
 class ViewController: UIViewController {
     var player: AVAudioPlayer!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5
         playSound(keyNote: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
     }
     
     
     func playSound(keyNote: String) {
-            let url = Bundle.main.url(forResource: keyNote, withExtension: "wav")
-            player = try! AVAudioPlayer(contentsOf: url!)
-            player.play()
-                    
-        }
+        let url = Bundle.main.url(forResource: keyNote, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
