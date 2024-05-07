e79ec44682f85487e40b9a6c7ffdbff2c3c77404
Frengky Gunawan <frengky.gg@gmail.com>
2024-05-07 23:08:11 +0700

    feat: add functionality for all button

--- 06 Xylophone/Xylophone/ViewController.swift
+++ 06 Xylophone/Xylophone/ViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
     @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(keyNote: sender.currentTitle!)
     }
     
-
+    
+    func playSound(keyNote: String) {
+            let url = Bundle.main.url(forResource: keyNote, withExtension: "wav")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
+                    
+        }
 }
 
