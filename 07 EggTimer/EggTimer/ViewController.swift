b31906187e40a481739520bc4ed39cacd53939b2
Frengky Gunawan <frengky.gg@gmail.com>
2024-05-09 17:12:30 +0700

    feat: add progress bar and sound

--- 07 EggTimer/EggTimer/ViewController.swift
+++ 07 EggTimer/EggTimer/ViewController.swift
@@ -7,31 +7,43 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressView: UIProgressView!
     
     let eggTimes = ["Soft": 5, "Medium": 420, "Hard": 720]
-    var secondsRemaining = 60
+    var totalTime = 60
+    var secondsPassed = 0
     var timer = Timer()
+    var player: AVAudioPlayer!
+    
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         timer.invalidate()
-        
         let hardness = sender.currentTitle!
         
-        secondsRemaining = eggTimes[hardness]!
+        progressView.progress = 0.0
+        secondsPassed = 0
+        titleLabel.text = hardness
+        
+        totalTime = eggTimes[hardness]!
         
         timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer(){
-        if secondsRemaining > 0 {
-            print("\(secondsRemaining) seconds.")
-            secondsRemaining -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            progressView.progress = Float(secondsPassed) / Float(totalTime)
         } else {
             timer.invalidate()
             titleLabel.text = "DONE!"
+            
+            let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+            player = try! AVAudioPlayer(contentsOf: url!)
+            player.play()
         }
     }
 }
